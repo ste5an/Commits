402deeb350565f8e4c1b3277f13d98d8dad0d5f6
stepan <etienne13@me.com>
2020-07-17 18:39:28 +0300

    Fourth commit

--- src/main/java/MyClass.java
+++ src/main/java/MyClass.java
@@ -3,5 +3,6 @@ public class MyClass {
     public static void main(String[] args) {
         System.out.println("My first commit");
         System.out.println("My third commit");
+        System.out.println("My third commit");
     }
 }
